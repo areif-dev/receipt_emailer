9539b4b9f8266a1dce82c6fa1c081548ce1a97a5
areif-dev <ajreifsnyder@protonmail.com>
2023-01-14 11:40:09 -0500

    Finish fix_invoice_number

--- receipt_emailer/__init__.py
+++ receipt_emailer/__init__.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import mm, inch
 
@@ -7,7 +8,7 @@ def verify_argv(*args) -> bool:
     """
     Ensure that the program received proper command line args. The program should
     be called with 3 additional arguments: the path to a text file containing
-    ABC invoice receipts to be converted to a PDF, the customer_id such as 
+    ABC invoice receipts to be converted to a PDF, the customer_id such as
     "SMIJO 0", and the email address of the customer, such as "johnsmith@domain.com"
 
     :param args: A list containing the arguments to verify. This should most likely
@@ -16,14 +17,16 @@ def verify_argv(*args) -> bool:
     """
 
     if len(args) < 4:
-        print("Missing arguments. Expected to find \"module_name\" \"path/to/txt/file\" \"customer_id\" \"customer_email\"")
+        print(
+            'Missing arguments. Expected to find "module_name" "path/to/txt/file" "customer_id" "customer_email"'
+        )
         return False
     else:
         try:
             with open(args[1], "r") as _:
                 pass
-        except FileNotFoundError: 
-            print(f"The file, \"{args[1]}\" does not exist")
+        except FileNotFoundError:
+            print(f'The file, "{args[1]}" does not exist')
             return False
         except IsADirectoryError:
             print(f"The path $args[1] is a directory and cannot be read")
@@ -32,16 +35,46 @@ def verify_argv(*args) -> bool:
             return True
 
 
+def fix_invoice_number(og_invoice_num: str, date: datetime) -> str:
+    """
+    ABC's 3-13 report truncates the front of invoice numbers if they are more
+    than 6 digits long. This function is a hacky way to add back the first digit
+    of those invoices that were truncated
+
+    :param og_invoice_num: String form of the original invoice number pulled from
+    the 3-13 report. It will have, at most, 6 digits
+
+    :param date: The date that is printed on the invoice. It should be in %m/%d/%y
+    format
+
+    :returns: Return the corrected invoice number with the beginning digit added
+    back if it was removed
+    """
+
+    PRE_MILL_INVOICE_DATE = (
+        "12/31/13"  # This is the date when RAC hit 1 million invoices
+    )
+    if (
+        date > datetime.strptime(PRE_MILL_INVOICE_DATE, "%m/%d/%y")
+        or date == datetime.strptime(PRE_MILL_INVOICE_DATE, "%m/%d/%y")
+        and og_invoice_num[0] == "0"
+    ):
+        return "1" + og_invoice_num
+
+    else:
+        return og_invoice_num
+
+
 def select_customer_invoices(invoices_str: str, customer_id: str):
     """
     Remove any invoice from invoices_str that was not rung out to customer_id
-    
+
     :param invoices_str: The raw, unformatted string data from the ABC 3-13 report
     :param customer_id: The unique 6 character code that ABC assigns to each customer
     """
 
     invoice_lines = invoices_str.split("\n")
-    
+
     index = len(invoice_lines)
     keep_invoice = False
     invoices_kept = []
@@ -49,31 +82,48 @@ def select_customer_invoices(invoices_str: str, customer_id: str):
         index -= 1
         line = invoice_lines[index]
 
-        # Generally, the customer id will appear around the 22nd line of the 
+        # Generally, the customer id will appear around the 22nd line of the
         # invoice. If the currect customer code is found, mark the invoice to be kept
         if line.strip().startswith(customer_id):
             keep_invoice = True
 
+            reversed_line = line[::-1]
+            invoice_date_str = reversed_line[:8][::-1]
+
+            # 3-13 prepends single digit dates with a space, so single digit 
+            # months will need to have that space removed to be passed to 
+            # strptime
+            if invoice_date_str[0] == " ":
+                invoice_date_str = invoice_date_str[1:]
+
+            invoice_date = datetime.strptime(invoice_date_str, "%m/%d/%y")
+
         # At this point, the loop has either reached the beginning of the invoice
-        # or a page break between a multipage invoice. If the word "continue" is 
+        # or a page break between a multipage invoice. If the word "continue" is
         # in the line, then the page is part of a long invoice, and nothing should
-        # be done yet. Otherwise, it is the start of an invoice, so break that 
+        # be done yet. Otherwise, it is the start of an invoice, so break that
         # invoice out
         elif line.strip().startswith("INVOICE #") and not "continue" in line.lower():
             if keep_invoice:
+
+                # ABC 3-13 only supports 6 digit invoice numbers, but RAC is up
+                lbs_sign_loc = line.find("#")
+                og_invoice_num = line[lbs_sign_loc + 1 :]
+                fixed_invoice_num = fix_invoice_number(og_invoice_num, invoice_date)
+                invoice_lines[index] = line[1 : lbs_sign_loc + 1] + fixed_invoice_num
                 invoices_kept.append("\n".join(invoice_lines[index:]))
                 keep_invoice = False
 
             del invoice_lines[index:]
 
-    # Because the invoices were read backwards, this list is in order of most 
-    # recent invoice first, oldest invoice last. 
+    # Because the invoices were read backwards, this list is in order of most
+    # recent invoice first, oldest invoice last.
     return invoices_kept[::-1]
 
 
 def txt_to_pdf(invoices: list[str], output: str):
     """
-    Convert a list of plaintext invoices from an ABC 3-13 report into a PDF. 
+    Convert a list of plaintext invoices from an ABC 3-13 report into a PDF.
     This should generally be called on the output of select_customer_invoices
 
     :param invoices: A list where each item is a complete invoice from ABC
@@ -85,7 +135,7 @@ def txt_to_pdf(invoices: list[str], output: str):
 
     # The default top of the page. Horizontal, Vertical
     START_POS = (5 * mm, 11 * inch - 10 * mm)
-    
+
     for invoice in invoices:
         hori_pos, vert_pos = START_POS
 
@@ -108,7 +158,7 @@ def txt_to_pdf(invoices: list[str], output: str):
 
 
 def main():
-    
+
     # Something is wrong with the command line args, so exit the program early
     if not verify_argv(*sys.argv):
         return
--- receipt_emailer/tests/test.py
+++ receipt_emailer/tests/test.py
@@ -4,10 +4,16 @@ from datetime import datetime
 
 
 class EmailerTestCase(unittest.TestCase):
-
     def test_verify_args(self):
         self.assertFalse(receipt_emailer.verify_argv())
-        self.assertTrue(receipt_emailer.verify_argv("module", "receipt_emailer/tests/single_invoice.txt", "SMIJO 0", "john.smith@domain.com"))
+        self.assertTrue(
+            receipt_emailer.verify_argv(
+                "module",
+                "receipt_emailer/tests/single_invoice.txt",
+                "SMIJO 0",
+                "john.smith@domain.com",
+            )
+        )
 
     def test_select_customer_invoices(self):
 
@@ -16,24 +22,36 @@ class EmailerTestCase(unittest.TestCase):
         with open("receipt_emailer/tests/single_invoice.txt", "r") as f:
             single_invoice = f.read()
 
-        self.assertEqual(len(receipt_emailer.select_customer_invoices(single_invoice, "SMIJO 0")), 1)
+        self.assertEqual(
+            len(receipt_emailer.select_customer_invoices(single_invoice, "SMIJO 0")), 1
+        )
 
         with open("receipt_emailer/tests/multiple_invoices.txt", "r") as f:
             multiple_invoices = f.read()
 
-        self.assertEqual(len(receipt_emailer.select_customer_invoices(multiple_invoices, "SMIJO 0")), 4)
-
+        self.assertEqual(
+            len(receipt_emailer.select_customer_invoices(multiple_invoices, "SMIJO 0")),
+            4,
+        )
 
     def test_fix_invoice_number(self):
         day_of_mill_invoice = datetime.strptime("12/31/13", "%m/%d/%y")
         day_before_invoice = datetime.strptime("12/30/13", "%m/%d/%y")
         day_after_invoice = datetime.strptime("1/1/14", "%m/%d/%y")
 
-        self.assertEqual(receipt_emailer.fix_invoice_number("000000", day_of_mill_invoice), "1000000")
-        self.assertEqual(receipt_emailer.fix_invoice_number("900000", day_of_mill_invoice), "900000")
-        self.assertEqual(receipt_emailer.fix_invoice_number("900000", day_before_invoice), "900000")
-        self.assertEqual(receipt_emailer.fix_invoice_number("900000", day_after_invoice), "1900000")
+        self.assertEqual(
+            receipt_emailer.fix_invoice_number("000000", day_of_mill_invoice), "1000000"
+        )
+        self.assertEqual(
+            receipt_emailer.fix_invoice_number("900000", day_of_mill_invoice), "900000"
+        )
+        self.assertEqual(
+            receipt_emailer.fix_invoice_number("900000", day_before_invoice), "900000"
+        )
+        self.assertEqual(
+            receipt_emailer.fix_invoice_number("900000", day_after_invoice), "1900000"
+        )
 
-    
+
 if __name__ == "__main__":
     unittest.main()
