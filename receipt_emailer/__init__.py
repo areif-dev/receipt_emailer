b01fcb0fbc36ec6d30eabe0b78cc52e7280d51d6
AJ Reifsnyder <53097078+areif-dev@users.noreply.github.com>
2023-02-01 12:34:15 -0500

    Add period to end of email message

--- receipt_emailer/__init__.py
+++ receipt_emailer/__init__.py
@@ -227,7 +227,7 @@ def email_pdf(pdf_path: str, to_email: str, start_inv_num: int, end_inv_num: int
 
     if start_inv_num == end_inv_num:
         subject = f"Reifsnyer's Ag Center Invoice {start_inv_num}"
-        msg = "Please see the attached PDF for a copy of your invoice"
+        msg = "Please see the attached PDF for a copy of your invoice."
     else:
         subject = f"Reifsnyder's Ag Center Invoices {start_inv_num} - {end_inv_num}"
         msg = "Please see the attached PDFs for copies of your invoices.\n\n"
